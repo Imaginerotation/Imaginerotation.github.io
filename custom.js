32a9f5d921ed6ac3bdf91146557ec2591ae113c2
Matt Policane <mattpolicane@gmail.com>
2016-11-15 14:51:00 +0000

    new

--- custom.js
+++ custom.js
@@ -4,12 +4,19 @@ $(window).on("orientationchange",function(){
     // $("p").css({"background-color":"yellow","font-size":"300%"});
     console.log("Portrait");
   }
-  else // Landscape
+  else if (window.orientation == "landscape-primary") // Landscape
   {
     // $("p").css({"background-color":"pink","font-size":"200%"});
     $('.layer1').hide();
 
     $('.layer2').show();
+  }
 
+  else if (window.orientation == "landscape-secondary") // Landscape
+  {
+    // $("p").css({"background-color":"pink","font-size":"200%"});
+    $('.layer1').hide();
+
+    $('.layer3').show();
   }
 });
