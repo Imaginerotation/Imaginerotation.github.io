1155775bb21092d44abe923e79316aebaf00cd6e
Matt Policane <mattpolicane@gmail.com>
2016-11-17 02:12:18 +0000

    new

--- custom.js
+++ custom.js
@@ -1,152 +1,85 @@
-$( document ).ready(function() {
-  $(".layer1").delay(0).fadeIn();
+$( document ).ready(function() //Function recognises change in phone orientation
+{
+  $(".layer1").delay(0).fadeIn(); //Initialise screen with a colour as all layers set to hidden in CSS
   console.log("The screen loaded");
 });
 
 $(window).on("orientationchange",function(){
-
-  if($('.layer1').is(':visible') || $('.layer1_horiz').is(':visible'))
+  if($('.layer1').is(':visible') || $('.layer1_horiz').is(':visible')) //Layer 1 is visible
 {
-  console.log("Layer is blue");
-  $('.layer1').fadeIn();
-  $('.layer1_horiz').hide();
+  $('.layer1').fadeIn(); //Show portrait version of layer
+  $('.layer1_horiz').hide(); //Remove horizontal version of layer
 
-  if (window.orientation == -90)
+  if (window.orientation == -90) //Phone rotated horizontally to the right
   {
-    console.log("moved right when blue");
-    $('.layer1').hide();
-    $('.layer3').hide();
-    $('.layer4').hide();
     $(".layer2_horiz").delay(0).fadeIn();
 
   }
 
-  else if (window.orientation == 90) // Landscape
+  else if (window.orientation == 90) //Phone rotated horizontally to the left
   {
-    console.log("moved left when blue");
-
-    $('.layer1').hide();
-    $('.layer2').hide();
-    $('.layer3').hide();
     $(".layer4_horiz").delay(0).fadeIn();
-
-
   }
   else
   {
     return;
   }
-
 }
 
 else if($('.layer2').is(':visible') || $('.layer2_horiz').is(':visible') )
 {
-console.log("Layer is yellow");
 $('.layer2').fadeIn();
 $('.layer2_horiz').hide();
 
-
-
-
 if (window.orientation == -90)
-
-
 {
-
-  console.log("moved right when yellow");
-  $('.layer2').hide();
-  $('.layer2_horiz').hide();
-  $('.layer1').hide();
-  $('.layer4').hide();
-  $(".layer3_horiz").delay(0).fadeIn();
-
-
+    $(".layer3_horiz").delay(0).fadeIn();
 }
 
 else if (window.orientation == 90) // Landscape
 {
-  console.log("moved left when yellow");
-
-  // $("p").css({"background-color":"pink","font-size":"200%"});
-  $('.layer2').hide();
-  $('.layer3').hide();
-  $('.layer4').hide();
   $(".layer1_horiz").delay(0).fadeIn();
-
-
 }
 else {
   return;
 }
-
 }
 
 else if($('.layer3').is(':visible')|| $('.layer3_horiz').is(':visible'))
 
 {
-  console.log("Layer is Red");
   $('.layer3').fadeIn();
   $('.layer3_horiz').hide();
 
 if (window.orientation == -90)
 {
-  console.log("moved right when red");
-
-  $('.layer3').hide();
-  $('.layer2').hide();
-  $('.layer1').hide();
   $(".layer4_horiz").delay(0).fadeIn();
-
-
 }
 
 else if (window.orientation == 90) // Landscape
 {
-  console.log("moved left when red");
-
-  // $("p").css({"background-color":"pink","font-size":"200%"});
-  $('.layer3').hide();
-  $('.layer4').hide();
-  $('.layer1').hide();
   $(".layer2_horiz").delay(0).fadeIn();
-
-
 }
 else {
   return;
 }
-
 }
 
 else if($('.layer4').is(':visible') || $('.layer4_horiz').is(':visible'))
 
 {
-  console.log("Layer is Green");
   $('.layer4').fadeIn();
   $('.layer4_horiz').hide();
 
 if (window.orientation == -90)
 {
-  console.log("moved right when green");
-
-  $('.layer4').hide();
-  $('.layer3').hide();
-  $('.layer2').hide();
   $(".layer1_horiz").delay(0).fadeIn();
-
-
 }
 
 else if (window.orientation == 90) // Landscape
 {
-  console.log("moved left when green");
-
-  $('.layer4').hide();
-  $('.layer2').hide();
-  $('.layer1').hide();
   $(".layer3_horiz").delay(0).fadeIn();
 
-
 }
 else {
   return;
@@ -156,7 +89,5 @@ else {
 else {
   return;
 }
-
 }
-
 );
