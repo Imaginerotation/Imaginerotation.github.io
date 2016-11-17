1bf6b0402facebc42b973690d04d40a11617977d
Matt Policane <mattpolicane@gmail.com>
2016-11-17 02:06:52 +0000

    new

---
+++ OLD/custom.js
@@ -0,0 +1,78 @@
+$(window).on("orientationchange",function(){
+
+  if(!$('.layer1').is(':visible'))
+{
+  console.log("Here");
+
+  if (window.orientation == 90)
+  {
+    $('.layer1').hide();
+    $('.layer2').show();
+  }
+  else if (window.orientation == -90) // Landscape
+  {
+    // $("p").css({"background-color":"pink","font-size":"200%"});
+    $('.layer1').hide();
+    $('.layer4').show();
+  }
+
+}
+
+else if(!$('.layer2').is(':visible'))
+{
+  console.log("Here");
+
+if (window.orientation == 90)
+{
+  $('.layer2').hide();
+  $('.layer3').show();
+}
+else if (window.orientation == -90) // Landscape
+{
+  // $("p").css({"background-color":"pink","font-size":"200%"});
+  $('.layer2').hide();
+  $('.layer1').show();
+}
+
+}
+
+else if(!$('.layer3').is(':visible'))
+{
+  console.log("Here");
+
+if (window.orientation == 90)
+{
+  $('.layer3').hide();
+  $('.layer4').show();
+}
+else if (window.orientation == -90) // Landscape
+{
+  // $("p").css({"background-color":"pink","font-size":"200%"});
+  $('.layer3').hide();
+  $('.layer2').show();
+}
+
+}
+
+else if(!$('.layer4').is(':visible'))
+{
+  console.log("Here");
+
+if (window.orientation == 90)
+{
+  $('.layer4').hide();
+  $('.layer1').show();
+}
+else if (window.orientation == -90) // Landscape
+{
+  // $("p").css({"background-color":"pink","font-size":"200%"});
+  $('.layer4').hide();
+  $('.layer3').show();
+}
+
+}
+
+
+
+
+});
